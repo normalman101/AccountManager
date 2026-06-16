21eb0ee03d4e2d03bfbc2d4b350d9cc23e4dca0c
Semyon <normal.man101@outlook.com>
2026-06-16 13:27:07 +0300

    Слегка изменил реализацию таблиц и добавил функцию с триггером

---
+++ AccountManager.Infrastructure/Migrations/001_create_enum_and_tables.sql
@@ -0,0 +1,23 @@
+﻿CREATE TYPE UserRole AS ENUM ('normal', 'admin');
+
+CREATE TABLE table_users
+(
+    id         UUID PRIMARY KEY  DEFAULT uuidv7(),
+    email      TEXT     NOT NULL,
+    role       UserRole NOT NULL,
+    is_deleted BOOLEAN  NOT NULL DEFAULT FALSE,
+    CONSTRAINT check_email CHECK ( length(email) > 0 )
+);
+
+CREATE UNIQUE INDEX unique_user_email
+ON table_users (email)
+WHERE (is_deleted = FALSE);
+
+CREATE TABLE table_passwords
+(
+    password TEXT NOT NULL,
+    user_id  UUID NOT NULL,
+    CONSTRAINT check_password CHECK ( length(password) > 0 ),
+    CONSTRAINT unique_user_id UNIQUE (user_id),
+    CONSTRAINT foreign_key_user_id FOREIGN KEY (user_id) REFERENCES table_users (id)
+);
---
+++ AccountManager.Infrastructure/Migrations/002_create_function_and_trigger.sql
@@ -0,0 +1,17 @@
+﻿CREATE FUNCTION function_soft_delete_user()
+    RETURNS TRIGGER AS
+$$
+BEGIN
+    UPDATE table_users
+    SET is_deleted = TRUE
+    WHERE id = OLD.id;
+    
+    RETURN NULL;
+END;
+$$ LANGUAGE plpgsql;
+
+CREATE TRIGGER trigger_soft_delete_user
+    BEFORE DELETE
+    ON table_users
+    FOR EACH ROW
+EXECUTE FUNCTION function_soft_delete_user();
