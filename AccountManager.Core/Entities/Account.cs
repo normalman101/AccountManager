63b7c112456c3e3c086454ab33071b865a7b2ac5
Semyon <normal.man101@outlook.com>
2026-06-16 17:40:51 +0300

    Удалил идентификатор сущности

--- AccountManager.Core/Entities/Account.cs
+++ AccountManager.Core/Entities/Account.cs
@@ -1,12 +1,10 @@
-﻿using System;
-using AccountManager.Core.Enums;
+﻿using AccountManager.Core.Enums;
 using AccountManager.Core.ValueObjects;
 
 namespace AccountManager.Core.Entities;
 
-public class User
+public class Account
 {
-    public Guid Id { init; get; }
     public required Email Email { init; get; }
     public required Password Password { init; get; }
     public Role Role { init; get; }
