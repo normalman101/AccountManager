b67b2e790f136cea54ff5e7d30e6b049eb9dbe05
Semyon <normal.man101@outlook.com>
2026-06-17 13:11:22 +0300

    Реализовал интерфейс сценариев использования и в сценариях реализовал его метод

--- AccountManager.Application/UseCases/AccountAuthorizationUseCase.cs
+++ AccountManager.Application/UseCases/AccountAuthorizationUseCase.cs
@@ -1,10 +1,11 @@
 ﻿using System.Threading.Tasks;
+using AccountManager.Application.Interface;
 using AccountManager.Core.Entities;
 using AccountManager.Infrastructure.Repositories;
 
 namespace AccountManager.Application.UseCases;
 
-public class AccountAuthorizationUseCase(AccountRepository accountRepository)
+public class AccountAuthorizationUseCase(AccountRepository accountRepository) : IExecutable<Account, Task<Account?>>
 {
     public async Task<Account?> Execute(Account account)
     {
