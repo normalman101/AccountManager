1394ce820e2ad79712ebadf09ca97269077adb7f
Semyon <normal.man101@outlook.com>
2026-06-17 22:08:10 +0300

    Реализовал сообщения

---
+++ AccountManager.Presentation/Messages/PageChangedMessage.cs
@@ -0,0 +1,7 @@
+﻿using AccountManager.Presentation.ViewModels;
+using Avalonia.Controls;
+using CommunityToolkit.Mvvm.Messaging.Messages;
+
+namespace AccountManager.Presentation.Messages;
+
+public class PageChangedMessage(ViewModelBase value) : ValueChangedMessage<ViewModelBase>(value);
---
+++ AccountManager.Presentation/Messages/UserLoggedInMessage.cs
@@ -0,0 +1,6 @@
+﻿using AccountManager.Core.Entities;
+using CommunityToolkit.Mvvm.Messaging.Messages;
+
+namespace AccountManager.Presentation.Messages;
+
+public class UserLoggedInMessage(Account value) : ValueChangedMessage<Account>(value);
