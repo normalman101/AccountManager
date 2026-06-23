3db54b8af78616ed4b2e43622313e87bdd3ebf77
Semyon <normal.man101@outlook.com>
2026-06-23 19:12:41 +0300

    Реализовал DTOs для слоя приложения и инфраструктуры

---
+++ AccountManager.Application/DTOs/AccountDto.cs
@@ -0,0 +1,10 @@
+﻿using AccountManager.Core.Enums;
+
+namespace AccountManager.Application.DTOs;
+
+public record AccountDto
+{
+    public required string Email { init; get; }
+    public required string Password { init; get; }
+    public required Role Role { init; get; }
+}
---
+++ AccountManager.Infrastructure/DTOs/AccountDto.cs
@@ -0,0 +1,10 @@
+﻿using AccountManager.Core.Enums;
+
+namespace AccountManager.Infrastructure.DTOs;
+
+public record AccountDto
+{
+    public required string Email { init; get; }
+    public required string Password { init; get; }
+    public required Role Role { init; get; }
+}
