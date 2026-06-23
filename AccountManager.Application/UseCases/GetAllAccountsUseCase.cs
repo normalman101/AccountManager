8f0902c32f31be295c1b3e6268553010ab4002f0
Semyon <normal.man101@outlook.com>
2026-06-23 19:17:45 +0300

    Реализовал UseCase для получения всех аккаунтов

---
+++ AccountManager.Application/UseCases/GetAllAccountsUseCase.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AccountManager.Application.DTOs;
+using AccountManager.Application.Interfaces;
+
+namespace AccountManager.Application.UseCases;
+
+public class GetAllAccountsUseCase(IAccountQueryRepository accountQueryRepository)
+{
+    public async Task<IEnumerable<AccountDto>> Execute()
+    {
+        var accounts = await accountQueryRepository.GetAll();
+
+        return accounts.Select(a => new AccountDto
+        {
+            Email = a.Email.Value,
+            Password = a.Password.Value,
+            Role = a.Role
+        });
+    }
+}
