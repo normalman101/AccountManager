c7cfc6e13aab2472bda0396eb84d4c92f4c30275
Semyon <normal.man101@outlook.com>
2026-06-23 19:11:47 +0300

    Переименовал файл

--- AccountManager.Application/Interface/IExecutable.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Threading.Tasks;
-using AccountManager.Application.DTOs;
-
-namespace AccountManager.Application.Interface;
-
-public interface IExecutable<out TValue, in TParameter>
-{
-    TValue Execute(TParameter request);
-}
---
+++ AccountManager.Application/Interfaces/IExecutable.cs
@@ -0,0 +1,6 @@
+﻿namespace AccountManager.Application.Interfaces;
+
+public interface IExecutable<out TValue, in TParameter>
+{
+    TValue Execute(TParameter request);
+}
